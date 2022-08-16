7c7bcff3d2da270906369743111bbed5c34bab40
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-08-16 14:24:41 -0400

    Actually 4th
    added functional buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
+    const baseURL = 'https://acnhapi.com/v1/fossils'
+    const dig = document.getElementById('dig-btn')
+    const donate = document.getElementById('donate-btn')
+    const sell = document.getElementById('sell-btn')
+    const speechBubble = document.getElementById('speech-bubble')
+
+    function renderFossil(fossil){
+        speechBubble.innerHTML = ''
+        let randomFossil = fossil[Math.floor(Math.random() * fossil.length)]
+        const img = document.createElement('img')
+        const p = document.createElement('p')
+        img.src = fossil.image_url
+        speechBubble.append(img, p)
+    }
+    dig.addEventListener('click', (e) => {
+       console.log('dig!')
+    })
+
+    sell.addEventListener('click', (e) => {
+        console.log('HOO HOO')
+    })
+
+    donate.addEventListener('click', (e) => {
+        console.log('donate!')
+    })
+
+})
